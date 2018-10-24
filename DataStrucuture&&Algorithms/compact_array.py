9d123cdf3fe588f4b94d3022be4ce231fea375ec
Master_Cloud <lsclike2016@hotmail.com>
2018-10-24 15:06:30 +1100

    think of compact array

---
+++ DataStrucuture&&Algorithms/compact_array.py
@@ -0,0 +1,8 @@
+import array
+import sys
+# the result is not the expectation
+primes = array.array('i', [2232, 5, 4, 2, 3, 5])
+test = [['hellosdsd'], 5, 4, 2, 3, 5]
+
+for t in test:
+    print(sys.getsizeof(t))
