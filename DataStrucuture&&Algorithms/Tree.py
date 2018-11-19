57b6dea58b8a6c4860f6599020202563eec69879
Master_Cloud <lsclike2016@hotmail.com>
2018-11-19 22:53:47 +1100

    add abstract tree

---
+++ DataStrucuture&&Algorithms/Tree.py
@@ -0,0 +1,36 @@
+class Tree:
+
+    class Position:
+
+        def element(self):
+            raise NotImplementedError('Must be implemented by subclass')
+
+        def __eq__(self, other):
+            raise NotImplementedError('Must be implemented by subclass')
+
+        def __ne__(self, other):
+            return not self.__eq__(other)
+
+    def root(self):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def parent(self, p):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def num_children(self, p):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def children(self):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def __len__(self):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def is_root(self, p):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def is_leaf(self, p):
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def is_empty(self):
+        return len(self) == 0
