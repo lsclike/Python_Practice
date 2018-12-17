7bc8887b1e802aa57dcde8db840ffc96262da973
Master_Cloud <lsclike2016@hotmail.com>
2018-12-17 16:12:15 +1100

    'find the longest substring via dictionary'

--- LeetCode/longest_substring_print.py
+++ LeetCode/longest_substring_print.py
@@ -1,13 +1,13 @@
 def longest_substring_without_repeating(string):
-    result_diction = []
+    result_diction = {}
+    result = []
     for i in range(len(string)):
-        result_diction.append(helper(string, i, result_diction))
+        helper(string, i, result_diction)
+    length = max(result_diction.keys())
+    for i in range(result_diction[length], result_diction[length] + length):
+        result.append(string[i])
 
-    longest_start_point = result_diction.index(max(result_diction))
-    longest_substring = []
-    for i in range(longest_start_point, longest_start_point + max(result_diction)):
-        longest_substring.append(string[i])
-    return ''.join(longest_substring)
+    return ''.join(result)
 
 
 def helper(string, start, diction):
@@ -16,11 +16,11 @@ def helper(string, start, diction):
         if string[i] not in seen:
             seen.add(string[i])
         else:
-            diction[i - start] = i
-            return
+            diction[i - start] = start
+            return None
     diction[len(string) - start] = start
-    return
+    return None
 
 if __name__ == '__main__':
     result_length = longest_substring_without_repeating('abcdee')
-    print(result_length)
+    print(result_length);
